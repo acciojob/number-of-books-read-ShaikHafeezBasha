47ec5f7436e44457a3d7806c6a2434341c277bf8
Shaik Hafeez Basha <115686162+ShaikHafeezBasha@users.noreply.github.com>
2023-04-20 06:21:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,13 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let count=0;
+	for(let i=0;i<library.length;i++){
+		if(lirary[i].readingStatus===true){
+			count++;
+		}
+	}
+	return count;
 };
 
 // Do not change the code below
